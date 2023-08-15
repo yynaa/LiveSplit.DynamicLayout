ffcb2083476051fd5e2a41d702e11b9c94f8b62c
yynauwu <xykaecutie@gmail.com>
2023-08-16 00:59:28 +0200

    Versioning

--- UI/Component/DynamicLayoutFactory.cs
+++ UI/Component/DynamicLayoutFactory.cs
@@ -19,6 +19,6 @@ namespace LiveSplit.UI.Components
 
         public string XMLURL => UpdateURL + "";
 
-        public Version Version => Version.Parse("1.1.0");
+        public Version Version => Version.Parse("1.1.1");
     }
 }
